aefb4c45ea4d32a054a05d82d739603861a5ac3f
NourEldin Ahmed Samir <NourEldin20201895@fci.helwan.edu.eg>
2022-06-14 18:32:19 +0200

    Adding events handling procedures

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
-  <div id="app">
-    <WeatherCard />
+  <div id="app" ref="app">
+    <WeatherCard :weather="weather" @toggle-theme="toggleTheme" />
     <div class="input-wrapper">
       <h2>Search</h2>
       <div class="flex align-center">
         <i class="ri-search-line"></i>
-        <input type="text" placeholder="Enter a city name" />
+        <input type="text" placeholder="Enter a city name" v-model="query" @keypress.enter="fetchWeather" />
       </div>
     </div>
   </div>
@@ -15,8 +15,28 @@
 import "./css/App.css";
 import "./css/Flex.css";
 import WeatherCard from "./components/WeatherCard.vue";
+
 export default {
   name: "App",
-  components: { WeatherCard },
+  components: {WeatherCard},
+  data() {
+    return {
+      url_base: "https://api.openweathermap.org/data/2.5/",
+      API_KEY: "f7c027b1b241ccf6d88a052db4a59c58",
+      query: "",
+      weather: {}
+    };
+  },
+  methods: {
+    fetchWeather() {
+      if (this.query == "") this.query = "Cairo";
+      fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.API_KEY}`)
+        .then((res) => res.json())
+        .then((data) => (this.weather = data));
+    },
+    toggleTheme() {
+      this.$refs["app"].toggleAttribute("light-mode");
+    },
+  },
 };
 </script>
--- src/components/WeatherCard.vue
+++ src/components/WeatherCard.vue
@@ -1,41 +1,97 @@
 <template>
-  <div class="weather-card flex flex-col justify-between">
-    <div class="flex-1 flex justify-between align-center">
-      <button id="toggler-btn">
+  <div
+    :class="
+      typeof weather.main != 'undefined' && weather.main.temp > 25
+        ? 'weather-card flex flex-col justify-between warm'
+        : 'weather-card flex flex-col justify-between'
+    "
+  >
+    <div
+      class="flex-1 flex justify-between align-center"
+      v-if="typeof weather.main != 'undefined'"
+    >
+      <button id="toggler-btn" @click="handleToggleTheme">
         <i class="ri-contrast-2-line"></i>
       </button>
       <div id="weather-city" class="flex align-center">
         <i class="ri-map-pin-2-line"></i>
-        <h3>Alexandria, EG</h3>
+        <h3>{{ weather.name }}, {{ weather.sys.country }}</h3>
       </div>
       <button><i class="ri-apps-2-line"></i></button>
     </div>
 
-    <div class="flex-2 flex flex-col align-center">
+    <div
+      class="flex-2 flex flex-col align-center"
+      v-if="typeof weather.main != 'undefined'"
+    >
       <div class="weather-icon flex justify-center align-center">
-        <i class="ri-sunny-line"></i>
+        <i
+          class="ri-moon-clear-line"
+          v-if="weather.weather[0].main.includes('Clear')"
+        ></i>
+        <i
+          class="ri-cloud-line"
+          v-if="weather.weather[0].main.includes('Clouds')"
+        ></i>
+        <i
+          class="ri-rainy-line"
+          v-if="weather.weather[0].main.includes('Rain')"
+        ></i>
+        <i
+          class="ri-snowy-line"
+          v-if="weather.weather[0].main.includes('Snow')"
+        ></i>
+        <i
+          class="ri-mist-line"
+          v-if="
+            weather.weather[0].main.includes('Mist') ||
+            weather.weather[0].main.includes('Dust') ||
+            weather.weather[0].main.includes('Drizzle') ||
+            weather.weather[0].main.includes('Fog')
+          "
+        ></i>
+        <i class="ri-sunny-line" v-else></i>
       </div>
-      <h1 id="weather-temp">26&deg;</h1>
-      <p id="weather-condition">Clear</p>
-      <small id="weather-date" class="muted">Monday, 17 May</small>
+      <h1 id="weather-temp">{{ Math.round(weather.main.temp) }}&deg;</h1>
+      <p id="weather-condition">{{ weather.weather[0].main }}</p>
+      <small id="weather-date" class="muted">{{ getDate() }}</small>
     </div>
 
-    <div class="flex-3 flex justify-between">
+    <div
+      class="flex-3 flex justify-between"
+      v-if="typeof weather.main != 'undefined'"
+    >
       <div id="wind-speed" class="flex flex-col align-center">
         <i class="ri-windy-line"></i>
-        <small>4.21 km/h</small>
+        <small>{{ weather.wind.speed }} km/h</small>
         <small class="muted">Wind</small>
       </div>
       <div id="humidity" class="flex flex-col align-center">
         <i class="ri-drop-line"></i>
-        <small>69%</small>
+        <small>{{ weather.main.humidity }}%</small>
         <small class="muted">Humidity</small>
       </div>
       <div id="pressure" class="flex flex-col align-center">
         <i class="ri-arrow-down-line"></i>
-        <small>1024 “Hg</small>
+        <small>{{ weather.main.pressure }} “Hg</small>
         <small class="muted">Pressure</small>
       </div>
     </div>
   </div>
 </template>
+
+<script>
+import moment from 'moment';
+export default {
+  name: "WeatherCard",
+  props: ["weather"],
+  methods: {
+    getDate() {
+      return moment().format("dddd, DD MMM");
+    },
+    handleToggleTheme() {
+      this.$emit("toggle-theme")
+    },
+  },
+};
+</script>
